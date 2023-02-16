7cf6a3826218ec7ffce5718ff6583ecf3e0ebf15
David Morgan <demorgan94@gmail.com>
2023-02-15 19:29:08 -0600

    Init repository

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./components/Form";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="container py-5">
+        <div className="row justify-content-md-center">
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
